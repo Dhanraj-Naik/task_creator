7da24385c8cb919ecd59d310bb9517c4d60f5ca3
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2024-02-29 12:59:06 +0530

    conditional handling

--- src/screens/useState/ArrayState.js
+++ src/screens/useState/ArrayState.js
@@ -3,22 +3,27 @@ import './arrayState.css';
 
 const ArrayState = () => {
     const [tasks, setTask] = useState([{ id: 1, name: 'Dhanraj', married: true }, { id: 2, name: 'Vipul', married: false }, { id: 3, name: 'Yogesh', married: true }]);
-
+    const [show, setShow] = useState(false);
     function deleteTask(id) {
         console.log('deleteTask: ', id)
         setTask(prevTask => (prevTask.filter(task => task.id !== id)))
+    }
+
+    function toggleHnndler() {
+        setShow(!show)
     }
 
     return (
         <>
             <div className="App">
                 <h1 className="arrayCount">{'LIST'}</h1>
+                <button className="toggle" onClick={toggleHnndler}>TOGGLE</button>
                 <ul>
-                    {tasks.map(task => (
-                        <li key={task.id}>
+                    {show && tasks.map(task => (
+                        (<li key={task.id} className={task.married ? 'married' : 'unmarried'}>
                             <span>{task.id} : {task.name}</span>
                             <button className='deleteTask' onClick={() => deleteTask(task.id)}>Delete</button>
-                        </li>
+                        </li>)
                     ))}
                 </ul>
             </div>
