ccb12bf8ada1fa7e3dacb7015d342ccf552a7f51
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2024-02-29 22:38:39 +0530

    working with form

--- src/components/AddTaskForm.js
+++ src/components/AddTaskForm.js
@@ -4,18 +4,37 @@ import { useState } from "react"
 const AddTaskForm = () => {
 
     const [user, setUser] = useState("")
+    const [married, setMarried] = useState(false)
 
     const handleChange = (event) => {
         setUser(event.target.value)
     }
     const handleReset = () => {
         setUser("")
+        setMarried(false)
+    }
+
+    const handleSelectChange = (event) => {
+        setMarried(Boolean(event.target.value))
+    }
+
+    const validateData = (event) => {
+        event.preventDefault()
+       
+        const personObject = {
+            id: Math.floor(Math.random() * 10000),
+            name: user,
+            married
+        }
+        console.log("hello ", personObject)
+        handleReset()
     }
 
 
     return (
         <section className="addtask">
             <form
+                onSubmit={validateData}
             >
                 <label htmlFor="task">Person Name: </label>
                 <input
@@ -27,6 +46,10 @@ const AddTaskForm = () => {
                     onChange={handleChange}
                     value={user}
                 />
+                <select onChange={handleSelectChange} value={married}>
+                    <option value={false}>UnMarried</option>
+                    <option value={true}>Married</option>
+                </select>
                 <button type="submit">Add Person</button>
                 <div>
                     <span onClick={handleReset} className="resetField">Reset</span>
