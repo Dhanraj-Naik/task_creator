d067718db47165a9e67a30b9d642132670395c99
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2024-02-29 18:05:10 +0530

    form and input fields

--- src/components/AddTaskForm.js
+++ src/components/AddTaskForm.js
@@ -1,13 +1,37 @@
+import { useState } from "react"
 
 
 const AddTaskForm = () => {
+
+    const [user, setUser] = useState("")
+
+    const handleChange = (event) => {
+        setUser(event.target.value)
+    }
+    const handleReset = () => {
+        setUser("")
+    }
+
+
     return (
         <section className="addtask">
             <form
             >
                 <label htmlFor="task">Person Name: </label>
-                <input type="text" name="task" id="task" placeholder="Person Name" />
+                <input
+                    type="text"
+                    name="task"
+                    id="task"
+                    placeholder="Person Name"
+                    autoComplete="off"
+                    onChange={handleChange}
+                    value={user}
+                />
                 <button type="submit">Add Person</button>
+                <div>
+                    <span onClick={handleReset} className="resetField">Reset</span>
+                    <p>{user}</p>
+                </div>
             </form>
         </section>
     )
