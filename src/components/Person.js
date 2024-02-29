6fe99507a3a66012bdb0f80e20e962e1afe37607
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2024-02-29 17:25:35 +0530

    Module Level Styling

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,11 +1,13 @@
-import React from 'react'
+// import React from 'react'
+import styles from './Person.module.css'
 
 function Person({ task, deleteTask }) {
     return (
         <li
             // key={task.id} 
-            className={task.married ? 'married' : 'unmarried'}>
-            <span>{task.id} : {task.name}</span>
+            className={task.married ? 'married' : 'unmarried'}
+            >
+            <span className={styles.title}>{task.id} : {task.name}</span>
             <button className='deleteTask' onClick={() => deleteTask(task.id)}>Delete</button>
         </li>)
 }
